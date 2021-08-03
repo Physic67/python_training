754a3b3ee71a3c4d38b23010192ffcb166c86b00
Physic67 <sivashin@gmail.com>
2021-08-03 16:12:34 +0300

    test add group 03.08.2021_9

--- test_add_contact.py
+++ test_add_contact.py
@@ -14,47 +14,46 @@ class UntitledTestCase(unittest.TestCase):
 
     def test_untitled_test_case(self):
         wd = self.wd
-        self.login(wd)
-        self.fill_names(wd)
-        self.add_address(wd)
+        self.login(wd, username="admin", password="secret")
+        self.fill_names(wd, "Edward", "Vampire", "Kallen")
+        self.add_address(wd, "Forks, Washington")
+        self.add_phones(wd, "495-1234567", "901-1234567")
 
-        self.add_phones(wd)
 
-    def add_phones(self, wd):
+    def add_phones(self, wd, homephone, mobilephone):
         # add telephones
-        wd.find_element_by_name("home").send_keys("495-1234567")
+        wd.find_element_by_name("home").send_keys(homephone)
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("901-1234567")
+        wd.find_element_by_name("mobile").send_keys(mobilephone)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        wd.find_element_by_link_text("home").click()
-        wd.find_element_by_link_text("Logout").click()
 
-    def add_address(self, wd):
+
+    def add_address(self, wd, address):
         # add address
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Forks, Washington")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
 
-    def fill_names(self, wd):
+    def fill_names(self, wd, firstname, middlename, lastname):
         # add name
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Edward")
+        wd.find_element_by_name("firstname").send_keys(firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Vampire")
+        wd.find_element_by_name("middlename").send_keys(middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Kallen")
+        wd.find_element_by_name("lastname").send_keys(lastname)
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         # Login
         driver.get("http://localhost/addressbook/")
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
     def tearDown(self):
