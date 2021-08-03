bac0e9ccc60dd1a09fb5ccc2f6c01ee02bde0391
Physic67 <sivashin@gmail.com>
2021-08-03 15:16:38 +0300

    test add group 03.08.2021_8

--- test_add_contact.py
+++ test_add_contact.py
@@ -9,44 +9,56 @@ import unittest, time, re
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
 
     def test_untitled_test_case(self):
-        driver = self.driver
+        wd = self.wd
+        self.login(wd)
+        self.fill_names(wd)
+        self.add_address(wd)
+
+        self.add_phones(wd)
+
+    def add_phones(self, wd):
+        # add telephones
+        wd.find_element_by_name("home").send_keys("495-1234567")
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys("901-1234567")
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        wd.find_element_by_link_text("home").click()
+        wd.find_element_by_link_text("Logout").click()
+
+    def add_address(self, wd):
+        # add address
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys("Forks, Washington")
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+
+    def fill_names(self, wd):
+        # add name
+        wd.find_element_by_link_text("add new").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys("Edward")
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys("Vampire")
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys("Kallen")
+
+    def login(self, driver):
         # Login
-        driver.get("http://localhost/addressbook/edit.php")
+        driver.get("http://localhost/addressbook/")
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys("admin")
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys("secret")
         driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("home").click()
-        driver.find_element_by_link_text("add new").click()
-        driver.find_element_by_name("firstname").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("Edward")
-        driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys("Vampire")
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("Kallen")
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("Forks, Washington")
-        driver.find_element_by_name("home").click()
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys("495-1234567")
-        driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys("901-1234567")
-        driver.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        driver.find_element_by_link_text("home").click()
-        driver.find_element_by_link_text("Logout").click()
-
-
-    
 
     def tearDown(self):
-        self.driver.quit()
+        self.wd.quit()
 
 
 if __name__ == "__main__":
