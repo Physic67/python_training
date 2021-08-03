73df66927831afee45fbec26a24f88dd2bf02b5b
Physic67 <sivashin@gmail.com>
2021-08-03 16:51:29 +0300

    test add group 03.08.2021_10

---
+++ contact.py
@@ -0,0 +1,11 @@
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, address, homephone, mobilephone):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.address = address
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+from contact import Contact
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
@@ -15,36 +11,37 @@ class UntitledTestCase(unittest.TestCase):
     def test_untitled_test_case(self):
         wd = self.wd
         self.login(wd, username="admin", password="secret")
-        self.fill_names(wd, "Edward", "Vampire", "Kallen")
-        self.add_address(wd, "Forks, Washington")
-        self.add_phones(wd, "495-1234567", "901-1234567")
+        x = Contact(firstname="Edward", middlename="Vampire", lastname="Kallen", address="Forks, Washington", homephone="495-1234567", mobilephone="901-1234567")
+        self.fill_names(wd, x)
+        self.add_address(wd, x)
+        self.add_phones(wd, x)
 
 
-    def add_phones(self, wd, homephone, mobilephone):
+    def add_phones(self, wd, contact):
         # add telephones
-        wd.find_element_by_name("home").send_keys(homephone)
+        wd.find_element_by_name("home").send_keys(contact.homephone)
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobilephone)
+        wd.find_element_by_name("mobile").send_keys(contact.mobilephone)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
 
-    def add_address(self, wd, address):
+    def add_address(self, wd, contact):
         # add address
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
 
-    def fill_names(self, wd, firstname, middlename, lastname):
+    def fill_names(self, wd, contact):
         # add name
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
 
     def login(self, driver, username, password):
         # Login
